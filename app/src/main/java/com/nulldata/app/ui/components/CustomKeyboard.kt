5e81e3437f039eb4fc3f0807bc3eebf5eaa059e8
Casp <cyberpunk2088@tutamail.com>
2026-05-18 01:59:22 +0200

    fix: move Sorani ؟ button to left of space bar
    RTL bottom bar: [123+] [.] [,] [؟] [Lang] [Space] [Done]

--- app/src/main/java/com/nulldata/app/ui/components/CustomKeyboard.kt
+++ app/src/main/java/com/nulldata/app/ui/components/CustomKeyboard.kt
@@ -361,6 +361,7 @@ private fun LetterLayout(
         if (lang.isRtl) {
             LetterKey('.', uppercase = false, onKey, keyBg, 1f, emptyMap())
             LetterKey('\u060C', uppercase = false, onKey, keyBg, 1f, emptyMap()) // Arabic comma
+            LetterKey('؟', uppercase = false, onKey, keyBg, 1f, emptyMap())
         } else {
             LetterKey(',', uppercase = false, onKey, keyBg, 1f, emptyMap())
             LetterKey('.', uppercase = false, onKey, keyBg, 1f, emptyMap())
@@ -377,7 +378,6 @@ private fun LetterLayout(
             LangPickerDropdown(showLangPicker, onDismiss = { onLangTap() }, selectLang)
         }
         if (lang.isRtl) {
-            LetterKey('؟', uppercase = false, onKey, keyBg, 1f, emptyMap())
             SpaceKey(keyBg) { onKey(' ') }
         } else {
             SpaceKey(keyBg) { onKey(' ') }
