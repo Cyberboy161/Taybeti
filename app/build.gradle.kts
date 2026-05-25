2ae31cd169c5f9d405b968ce2273380f25dda8ba
Casp <cyberpunk2088@tutamail.com>
2026-05-25 03:33:32 +0200

    v1.0.56

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.taybeti.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 50
-        versionName = "1.0.55"
+        versionCode = 51
+        versionName = "1.0.56"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
